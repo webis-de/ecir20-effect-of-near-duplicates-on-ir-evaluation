098dc1ca707abec9eb16833a6bc5c5c2c517547e
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-03-21 14:01:54 +0100

    make bloom-filter test more realistic

--- src/main/java/de/webis/trec_ndd/spark/SparkBuildBloomFilter.java
+++ src/main/java/de/webis/trec_ndd/spark/SparkBuildBloomFilter.java
@@ -1,38 +1,70 @@
 package de.webis.trec_ndd.spark;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
 
 import com.google.common.collect.Iterators;
+import com.google.common.hash.BloomFilter;
 
-import de.webis.trec_ndd.util.NGramms.Word8Gramm;
+import de.webis.trec_ndd.similarity.MD5;
 import lombok.SneakyThrows;
+import scala.Tuple3;
 
 public class SparkBuildBloomFilter {
 	public static void main(String[] args) {
 		try (JavaSparkContext context = context()) {
-			JavaRDD<String> docs = context.textFile("tmp-analysis-of-judged-cw09-cw12-docs");
+			BloomFilter<String> bf = bf("tmp-analysis-of-judged-cw09-cw12-docs", context);
+			List<String> dummyElements = new ArrayList<>();
+			for(int i=0; i<10000; i++) {
+				dummyElements.add(MD5.md5hash(""+ i));
+			}
 			
-			docs.flatMap(i -> bla(i))
-				.saveAsTextFile("tmp-bloom-filter");
+			context.parallelize(dummyElements)
+				.map(i -> tmpBla(bf, i))
+				.saveAsTextFile("bloom-filter-result-tmp-test");
 		}
 	}
+	
+	private static Tuple3<String, Boolean, Long> tmpBla(BloomFilter<String> bf, String value) {
+		long start = System.currentTimeMillis();
+		boolean mightContain = bf.mightContain(value);
+		long end = System.currentTimeMillis();
+		
+		return new Tuple3<>(value, mightContain, end-start);
+	}
+	
+	private static final BloomFilter<String> bf(String dir, JavaSparkContext context) {
+		Collection<String> elements = context.textFile(dir)
+				.flatMap(i -> bla(i))
+				.distinct()
+				.collect();
+		elements = new HashSet<String>(elements);
+		
+		BloomFilter<String> bf = BloomFilter.create(null, elements.size(), 1.0e-8);
+		for(String element: elements) {
+			bf.put(element);
+		}
+		
+		return bf;
+	}
 
-	@SuppressWarnings("unchecked")
 	@SneakyThrows
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private static Iterator<String> bla(String str) {
 		Map<String, Object> parsed = new ObjectMapper().readValue(str, new TypeReference<Map<String, Object>>() {});
-		Set<Word8Gramm> word8gramms = (Set<Word8Gramm>) parsed.get("word8gramms");
-		
-		return Iterators.transform(word8gramms.iterator(), i -> i.getMd5Hash());
+		Set<Map<String, String>> word8gramms = new HashSet<Map<String, String>>((Collection)parsed.get("word8gramms"));
+
+		return Iterators.transform(word8gramms.iterator(), i -> i.get("md5Hash"));
 	}
 	
 	private static JavaSparkContext context() {
