6ac89c91e982e1e98879481bd9df5c19f58b1338
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2020-03-23 09:17:20 +0100

    Prepare Bloom-Filter Run with ClueWeb09

--- src/main/java/de/webis/trec_ndd/spark/SparkBuildBloomFilter.java
+++ src/main/java/de/webis/trec_ndd/spark/SparkBuildBloomFilter.java
@@ -1,13 +1,15 @@
 package de.webis.trec_ndd.spark;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
@@ -16,37 +18,32 @@ import com.google.common.hash.BloomFilter;
 import com.google.common.hash.Funnel;
 import com.google.common.hash.Funnels;
 
-import de.webis.trec_ndd.similarity.MD5;
+import avro.shaded.com.google.common.collect.Iterators;
+import de.webis.trec_ndd.trec_collections.AnseriniCollectionReader;
+import de.webis.trec_ndd.trec_collections.CollectionConfiguration;
+import de.webis.trec_ndd.trec_collections.CollectionConfiguration.TrecCollections;
+import de.webis.trec_ndd.util.NGramms;
+import de.webis.trec_ndd.util.NGramms.Word8Gramm;
+import de.webis.trec_ndd.trec_collections.CollectionDocument;
+import io.anserini.collection.ClueWeb09Collection.Document;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.experimental.Wither;
 import scala.Tuple2;
-import scala.Tuple3;
 
 public class SparkBuildBloomFilter {
+	private static final TrecCollections COLLECTION = TrecCollections.CLUEWEB09;
+	
 	public static void main(String[] args) {
 		try (JavaSparkContext context = context()) {
 			List<Tuple2<String, BloomFilter<CharSequence>>> bf = bf("tmp-analysis-of-judged-cw09-cw12-docs", context);
-			List<String> dummyElements = new ArrayList<>();
-			for(int i=0; i<1000000; i++) {
-				dummyElements.add(MD5.md5hash(""+ i));
-			}
+			JavaRDD<CandidateDocumentForTopic> documents = collectionDocuments(context, COLLECTION, bf);
 			
-			context.parallelize(dummyElements)
-				.map(i -> tmpBla(bf, i))
-				.saveAsTextFile("bloom-filter-result-tmp-test");
+			documents.map(i -> i.toString())
+				.saveAsTextFile("tmp-deduplication-candidates-for-cw09");
 		}
-	}
-	
-	private static Tuple3<String, List<String>, Long> tmpBla(List<Tuple2<String, BloomFilter<CharSequence>>> bf, String value) {
-		long start = System.currentTimeMillis();
-		List<String> possibleMatches = new ArrayList<>();
-		for(Tuple2<String, BloomFilter<CharSequence>> t: bf) {
-			if(t._2.mightContain(value)) {
-				possibleMatches.add(t._1());
-			}
-		}
-		long end = System.currentTimeMillis();
-
-		return new Tuple3<>(value, possibleMatches, end-start);
 	}
 	
 	private static final List<Tuple2<String, BloomFilter<CharSequence>>> bf(String dir, JavaSparkContext context) {
@@ -69,6 +66,64 @@ public class SparkBuildBloomFilter {
 		}
 		
 		return new Tuple2((String) parsed.get("topic"), bf);
+	}
+	
+	private static JavaRDD<CandidateDocumentForTopic> collectionDocuments(JavaSparkContext context, CollectionConfiguration collection, List<Tuple2<String, BloomFilter<CharSequence>>> bf) {
+		AnseriniCollectionReader<Document> acr = new AnseriniCollectionReader<Document>(collection);
+		List<String> segmentPaths = acr.segmentPaths();
+
+		return context.parallelize(segmentPaths)
+				.flatMap(s -> calculateItForIterator(acr.collectionDocumentsInPath(s), bf))
+				.flatMap(i -> i.iterator());
+	}
+	
+	private static Iterator<List<CandidateDocumentForTopic>> calculateItForIterator(Iterator<CollectionDocument> docs, List<Tuple2<String, BloomFilter<CharSequence>>> bf) {
+		return Iterators.transform(docs, i -> calculateIt(i, bf));
+	}
+	
+	private static List<CandidateDocumentForTopic> calculateIt(CollectionDocument doc, List<Tuple2<String, BloomFilter<CharSequence>>> bf) {
+		Set<Word8Gramm> word8Gramms = new HashSet<>(NGramms.build8Gramms(doc.getFullyCanonicalizedContent()));
+		
+		return bf.stream()
+				.map(i -> toCandidate(doc, word8Gramms, i))
+				.filter(i -> i.lowerBoundOnMatching8Gramms()>= 0.4)
+				.collect(Collectors.toList());
+	}
+	
+	private static CandidateDocumentForTopic toCandidate(CollectionDocument doc, Set<Word8Gramm> word8Gramms, Tuple2<String, BloomFilter<CharSequence>> bf) {
+		int mightMatching8Gramms = 0;
+		
+		for(Word8Gramm word8Gramm: word8Gramms) {
+			if(bf._2.mightContain(word8Gramm.getMd5Hash())) {
+				mightMatching8Gramms++;
+			}
+		}
+		
+		return new CandidateDocumentForTopic()
+				.withCollection(COLLECTION.name())
+				.withDocumentId(doc.getId())
+				.withTopic(bf._1())
+				.withWord8Gramms(word8Gramms.size())
+				.withMightMatching8Gramms(mightMatching8Gramms);
+	}
+	
+	@Data
+	@Wither
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class CandidateDocumentForTopic {
+		private String topic, documentId, collection;
+		private int word8Gramms, mightMatching8Gramms;
+		
+		public double lowerBoundOnMatching8Gramms() {
+			return ((double) mightMatching8Gramms)/((double) word8Gramms);
+		}
+		
+		@Override
+		@SneakyThrows
+		public String toString() {
+			return new ObjectMapper().writeValueAsString(this);
+		}
 	}
 	
 	private static JavaSparkContext context() {
