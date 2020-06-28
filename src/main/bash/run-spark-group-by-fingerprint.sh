ca85f1c5335c9331534bc130474d3b5e94746b11
najtin <42641080+najtin@users.noreply.github.com>
2020-06-28 22:51:04 +0200

    fix #16

--- src/main/bash/run-spark-group-by-fingerprint.sh
+++ src/main/bash/run-spark-group-by-fingerprint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 spark-submit \
+	--deploy-mode cluster \
 	--class de.webis.trec_ndd.spark.SparkGroupByFingerprint \
 	target/trec-ndd-1.0-SNAPSHOT-jar-with-dependencies.jar \
 	--documentSelection JUDGED \
